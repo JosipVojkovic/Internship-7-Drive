9d2a86d11a5a2a88a03e2a3ff930fbf43ddd5623
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2024-12-27 01:14:12 +0100

    Add Factories: DbContextFactory and RepositoryFactory

---
+++ DriveApp/DriveApp.Domain/Factories/DbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using DriveApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace DriveApp.Domain.Factories
+{
+    public static class DbContextFactory
+    {
+        public static DriveAppDbContext GetDriveAppDbContext()
+        {
+            var options = new DbContextOptionsBuilder()
+                .UseNpgsql(ConfigurationManager.ConnectionStrings["DriveApp"].ConnectionString)
+                .Options;
+
+            return new DriveAppDbContext(options);
+        }
+    }
+}
---
+++ DriveApp/DriveApp.Domain/Factories/RepositoryFactory.cs
@@ -0,0 +1,16 @@
+﻿using DriveApp.Domain.Repositories;
+
+namespace DriveApp.Domain.Factories
+{
+    public class RepositoryFactory
+    {
+        public static TRepository Create<TRepository>()
+        where TRepository : BaseRepository
+        {
+            var dbContext = DbContextFactory.GetDriveAppDbContext();
+            var repositoryInstance = Activator.CreateInstance(typeof(TRepository), dbContext) as TRepository;
+
+            return repositoryInstance!;
+        }
+    }
+}
